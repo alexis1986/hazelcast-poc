949881712d6cc22fd62cbf16158ecdc49bc46817
Alexis <alexis.castellano@gmail.com>
2025-10-05 20:02:47 -0300

    agrego los custom hooks

---
+++ web/src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useContacts } from './useContacts';
+export { useSnackbar } from './useSnackbar';
+export type { SnackbarSeverity } from './useSnackbar';
+export { useCacheStats } from './useCacheStats';
